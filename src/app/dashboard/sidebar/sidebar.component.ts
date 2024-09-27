c3d401c3e49161b65a86ee9fff6433afb9472ebe
yosrasejjil <95542447+yosrasejjil@users.noreply.github.com>
2024-09-27 02:11:44 +0100

    trouchik

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
- 
+  constructor(private router: Router) {}
+
+  navigateAndReload(route: string) {
+    this.router.navigate([route]).then(() => {
+      window.location.reload(); // Reload the page after navigation
+    });
+  }
 }
